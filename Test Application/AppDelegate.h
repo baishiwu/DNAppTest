ae2a8ebbe9b4fe374ddb8def07ca795ce9c871b4
Baishi Wu <baishi@gmail.com>
2014-08-04 17:53:53 -0400

    Initial Commit

---
+++ Test Application/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  Test Application
+//
+//  Created by Baishi Wu on 8/4/14.
+//  Copyright (c) 2014 Baishi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ Test Application/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Test Application
+//
+//  Created by Baishi Wu on 8/4/14.
+//  Copyright (c) 2014 Baishi Wu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
